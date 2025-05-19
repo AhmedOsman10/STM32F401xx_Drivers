21497dfcd9a69cf3c797ad14c7249b9d364ffdf7
AhmedOsman10 <Ahmed.Derwish10@gmail.com>
2025-05-19 19:12:53 +1000

    modified the GPIO folder name

---
+++ MCAL/GPIO/GPIO_Config.h
@@ -0,0 +1,13 @@
+/*
+ * GPIO_Config.h
+ *
+ *  Created on: May 6, 2025
+ *      Author: Ahmed
+ */
+
+#ifndef GPIO_GPIO_CONFIG_H_
+#define GPIO_GPIO_CONFIG_H_
+
+
+
+#endif /* GPIO_GPIO_CONFIG_H_ */
